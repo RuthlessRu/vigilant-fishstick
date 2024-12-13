d4ce8c83f7ccbcfbd685c8a41728ae61c854e265
Adam Kanoun <adam.kanoun@gmail.com>
2024-12-13 08:56:18 -0500

    Added baseline resnet model + fixed comments

--- notebooks/preprocess_dataset.py
+++ notebooks/preprocess_dataset.py
@@ -181,7 +181,6 @@ def parse_cremad(dataset_dir, demographics_file):
 
     return file_paths, labels    
 
-
 if __name__ == "__main__":
     cremad_dir = os.path.expanduser("~/.cache/kagglehub/datasets/ejlok1/cremad")
     ravdess_dir = os.path.expanduser("~/.cache/kagglehub/datasets/uwrfkaggler")
