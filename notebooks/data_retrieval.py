216cb352dd9d7cedc7e4460cdd788cfddc1f9799
Ru <h.axcil4220@outlook.com>
2024-12-14 02:32:08 -0500

    Updated Readme

--- notebooks/data_retrieval.py
+++ notebooks/data_retrieval.py
@@ -4,12 +4,12 @@ import torchaudio
 
 # Download TESS DATASET
 
-# tess_dir = os.path.expanduser("~/.cache/kagglehub/datasets/ejlok1")
-# if not os.path.exists(tess_dir):
-#     kagglehub.dataset_download("ejlok1/toronto-emotional-speech-set-tess")
+tess_dir = os.path.expanduser("~/.cache/kagglehub/datasets/ejlok1")
+if not os.path.exists(tess_dir):
+    kagglehub.dataset_download("ejlok1/toronto-emotional-speech-set-tess")
 
-# rav_dir = os.path.expanduser("~/.cache/kagglehub/datasets/uwrfkaggler")
-# if not os.path.exists(rav_dir):    
-#     kagglehub.dataset_download("uwrfkaggler/ravdess-emotional-speech-audio")
+rav_dir = os.path.expanduser("~/.cache/kagglehub/datasets/uwrfkaggler")
+if not os.path.exists(rav_dir):    
+    kagglehub.dataset_download("uwrfkaggler/ravdess-emotional-speech-audio")
 
-#kagglehub.dataset_download("ejlok1/cremad")
+kagglehub.dataset_download("ejlok1/cremad")
